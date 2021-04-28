8747e1d03e24e2460a76fb52b85cc4e504fefe5b
Tony Shifflett <tonyshifflett@tonys-mbp.lan>
2021-04-28 14:54:57 -0500

    created api call in Pokedex.js, set pokemon array, printed list of names to prove data is received and passed properly

--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,10 +1,35 @@
-
+import React, {useState, useEffect} from 'react'
 
 const Pokedex =()=>{
-
+    const [pokemon, setPokemon] = useState(null)
+    const catchPokemon = async()=>{
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=00&limit=151')
+    const data = await response.json()
+    setPokemon(data)
+    }
+   
+    console.log("working")
+    
+    useEffect(()=>{catchPokemon()}, [])
     return(
-        <h1>Pokedex Component</h1>
+        <div>
+            {
+                pokemon != null ?
+                pokemon.results.map((poke)=>{
+                    const {name, url} = poke;
+                    return (
+                        <h3>{name}</h3>
+                    )
+                })
+                :
+                <div>loading</div>
+
+            }
+          
+        </div>
     )
+            
+    
 }
 
 export default Pokedex
